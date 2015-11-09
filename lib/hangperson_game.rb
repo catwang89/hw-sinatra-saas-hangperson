c0ae7c62e78a5aa34aa0708bcd11405d43cc3a06
catwang89 <catwang89@gmail.com>
2015-11-09 22:53:58 +1100

    Ruby slove problem set 1#

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,14 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_reader :word, :guesses, :wrong_guesses
+  
+  @@get_random_word = ''
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses =''
   end
 
   def self.get_random_word
@@ -18,5 +24,32 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
+  
+  #guess 
+  def guess(guess_word)
+       raise ArgumentError, 'Argument is not a string' unless guess_word.is_a?String
+       raise ArgumentError, 'Argument is empty' unless !guess_word.empty?
+       raise ArgumentError, 'Argument '
+       
+       if @guesses != guess_word && @wrong_guesses !=guess_word && guess_word != guess_word.upcase
+          if @word.include? guess_word
+              @guesses = guess_word
+          else
+            @wrong_guesses = guess_word
+          end
+       else
+         return false
+       end
+  #end of guess method
+  end
+  
+   #begin
+    #guess('')
+    #guess_word =~ /\A\d+\Z/
+   #rescue ArgumentError
+     #puts 'invalid argument'
+   #end
+
+#
 
 end
